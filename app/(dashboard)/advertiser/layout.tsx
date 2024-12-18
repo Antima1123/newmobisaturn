65bc65e3290105bc150d1a2344c95abaa61439b0
Antima1123 <antimasinghthakur4@gmail.com>
2024-12-18 15:54:05 +0530

    bugs fixes

--- app/(dashboard)/advertiser/layout.tsx
+++ app/(dashboard)/advertiser/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "AdByteHub for Advertisers - Maximize Your Ad Spend and Reach Your Target Audience",
+    title: "Advertisers - Optimize Your Ad Spend and Reach More Customers",
     description: "Use the advertiser options offered by AdByteHub to increase your reach. Utilize our state-of-the-art advertising tools and tactics to engage your target audience, increase engagement, and provide quantifiable outcomes.",
   };
 const AboutUsPageLayout =({children}: {children: React.ReactNode}) => {
