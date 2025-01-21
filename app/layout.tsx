6fc98d74c6e954fdcc34ccc8ab76cf29e4095347
Antima Singh <160607543+Antima1123@users.noreply.github.com>
2025-01-21 16:25:47 +0530

    consent 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,6 +99,29 @@ export default function RootLayout({
           type="text/javascript"
           async
         ></script>
+        <script
+          data-cookieconsent="ignore"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {
+                  dataLayer.push(arguments);
+              }
+              gtag("consent", "default", {
+                  ad_personalization: "denied",
+                  ad_storage: "denied",
+                  ad_user_data: "denied",
+                  analytics_storage: "denied",
+                  functionality_storage: "denied",
+                  personalization_storage: "denied",
+                  security_storage: "granted",
+                  wait_for_update: 500,
+              });
+              gtag("set", "ads_data_redaction", true);
+              gtag("set", "url_passthrough", false);
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
